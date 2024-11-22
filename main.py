e92ec9074f7c4ca15923c019e1487bb3fa00d013
shahramsamar <shahramsamar2010@gmail.com>
2024-11-22 09:34:27 +0330

    scheduler implementation

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from fastapi import FastAPI, BackgroundTasks
 from time import sleep
 import threading
 from random import randint
+from apscheduler.schedulers.background import BackgroundScheduler
 
+
+scheduler = BackgroundScheduler()
 app = FastAPI()
 
 """ this is a simple sleep implementation"""
@@ -16,7 +19,6 @@ async def test_sleep():
 
 
 """ this is a simple async implementation"""
-
 def trigger_process():
     print("Process triggered")
     sleep(10)
@@ -32,8 +34,6 @@ async def test_async():
 
 
 """ this is a simple BackgroundTasks implementation"""
-
-
 def trigger_process(num):
     print("Process triggered", num)
     sleep(randint(2, 10))
@@ -41,10 +41,33 @@ def trigger_process(num):
     
 num = 1
 
-
 @app.get("/BackgroundTasks")
 async def test_BackgroundTasks(background_tasks:BackgroundTasks):
     global num
     background_tasks.add_task(trigger_process, num)
     num += 1
     return {"detail": "done"}
+
+
+""" this is a simple scheduler implementation"""
+num = 1
+def trigger_process():
+    global num
+    print("Process triggered:", num)
+    sleep(randint(2, 10))
+    print("Process finished:",num)
+    num += 1
+
+    
+    
+@app.get("/scheduler")
+async def test_scheduler(background_tasks:BackgroundTasks):
+    global num
+    background_tasks.add_task(trigger_process, num)
+    num += 1
+    return {"detail": "done"}
+
+
+
+scheduler.add_job(trigger_process, 'interval', seconds=10)    
+scheduler.start()    
