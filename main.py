10b024d653d29d5c9bafd4903f4870306b9b1aa4
shahramsamar <shahramsamar2010@gmail.com>
2024-11-22 09:12:47 +0330

    BackgroundTasks implementation

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, BackgroundTasks
 
 
 from time import sleep
 import threading
+from random import randint
 
 app = FastAPI()
 
@@ -26,4 +27,24 @@ def trigger_process():
 async def test_async():
     task =threading.Thread(target=trigger_process)
     task.start()
+    return {"detail": "done"}
+
+
+
+""" this is a simple BackgroundTasks implementation"""
+
+
+def trigger_process(num):
+    print("Process triggered", num)
+    sleep(randint(2, 10))
+    print("Process finished", num)
+    
+num = 1
+
+
+@app.get("/BackgroundTasks")
+async def test_BackgroundTasks(background_tasks:BackgroundTasks):
+    global num
+    background_tasks.add_task(trigger_process, num)
+    num += 1
     return {"detail": "done"}
